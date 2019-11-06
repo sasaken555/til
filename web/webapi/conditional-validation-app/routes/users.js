5f5b418cca8aeb8462375af26a11ad19b39ab9f2
sasaken555 <accelerk@gmail.com>
2019-11-07 00:19:14 +0900

    express-validatorで単項目チェック

--- web/webapi/conditional-validation-app/routes/users.js
+++ web/webapi/conditional-validation-app/routes/users.js
@@ -1,9 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const uuid = require("uuid/v4");
+
+const USER_TYPE_CONSUMER = "1";
+const USER_TYPE_COMPANY = "2";
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", (req, res) => {
   res.send("respond with a resource");
 });
+
+router.post(
+  "/",
+  [
+    check("userName").exists(),
+    check("age")
+      .exists()
+      .isInt(),
+    check("email").isEmail(),
+    check("userType")
+      .exists()
+      .isIn([USER_TYPE_CONSUMER, USER_TYPE_COMPANY])
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    const userId = uuid();
+    const { userName, nickName, age, email, userType } = req.body;
+    const ts = new Date().toISOString();
+
+    const user = {
+      userId,
+      userName,
+      nickName,
+      age,
+      email,
+      userType,
+      createdDate: ts,
+      updatedDate: ts
+    };
+    console.log("user created:", user);
+
+    res.status(201).json({ userId });
+  }
+);
 
 module.exports = router;
